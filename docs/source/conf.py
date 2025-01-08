95f85d41a7cff6ab8989221410b1cd07e37ed89a
santosg572 <santosg572@gmail.com>
2025-01-08 12:08:24 -0600

    Test comité

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,8 +3,8 @@
 # -- Project information
 
 project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+copyright = '2025, L. Concha, L. González-Santos'
+author = 'L. Concha, L. González-Santos'
 
 release = '0.1'
 version = '0.1.0'
---
+++ docs/source/copy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+fil=$1
+
+cp ../../../clusterio2/docs/source/$fil .
+
+
