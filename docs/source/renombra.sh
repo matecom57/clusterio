fdf4921607bb77e3aac179c7ee331a36bb7b15b0
santosg572 <santosg572@gmail.com>
2025-01-13 16:08:32 -0600

    Test comité

--- docs/source/renombra.sh
+++ docs/source/renombra.sh
@@ -2,13 +2,13 @@
 
 fil=$1
 
-filn=$(echo $fil | sed 's/_-/-/g')
-
-filn=$(echo $filn | sed 's/_/-/g')
-
+filn=$(echo $fil | sed 's/á/a/g')
+filn=$(echo $filn | sed 's/é/e/g')
+filn=$(echo $filn | sed 's/í/i/g')
 filn=$(echo $filn | sed 's/ó/o/g')
-
-filn=$(echo $filn | sed 's/á/a/g')
+filn=$(echo $filn | sed 's/ú/u/g')
+filn=$(echo $filn | sed 's/_-/-/g')
+filn=$(echo $filn | sed 's/_/-/g')
 
 echo $filn
 
